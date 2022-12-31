af3eb5900a31c34bb51b4a8435bdbc53bd2eccba
Manas Ranjan Adha <118095682+itsmanas-ra@users.noreply.github.com>
2022-12-31 23:30:19 +0530

    fix: event handing input output added

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
 let btnTranslate = document.querySelector("#btn-translate");
 let txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
 // console.log(txtInput);
 
-btnTranslate.addEventListener("click", function clickEventHandler() {
+// btnTranslate.addEventListener("click", function clickEventHandler() {
+//     console.log("clicked!");
+//     console.log("input", txtInput.value);
+// })
+
+function clickHandler() {
     console.log("clicked!");
     console.log("input", txtInput.value);
-})
+   outputDiv.innerText = "ajsjsjsjsjsjsj  " + txtInput.value;
+};
+
+
+
+btnTranslate.addEventListener("click", clickHandler);
+
+btnTranslate.addEventListener("click", clickHandler);
