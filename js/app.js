07139094178323b5807e7b20470460ece10d8def
Manas Ranjan Adha <118095682+codewithmanas@users.noreply.github.com>
2023-01-24 16:48:08 +0530

    live exercise: querySelector

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 
 let btnTranslate = document.querySelector("#btn-translate");
 let txtInput = document.querySelector("#txt-input");
+let txtArea = document.querySelector("textarea");
+let docBody = document.querySelector("body");
 
 
 console.log(txtInput);
+console.log(txtArea);
 
 
 // console.log(btnTranslate);
@@ -12,6 +15,10 @@ console.log(txtInput);
 function clickHandler(){
     console.log("clicked!");
     console.log("input",txtInput.value);
+    console.log("input",btnTranslate.value);
+    console.log("input",txtArea.style.backgroundColor = "yellow");
+    console.log("input",docBody.style.backgroundColor = "green");
+    console.log("input",txtArea.style.marginLeft = "0");
 
 }
 
