d6467f159484cd484a7a854a880642b5a81857a6
Manas Ranjan Adha <118095682+codewithmanas@users.noreply.github.com>
2023-01-24 11:12:45 +0530

    ex02 done: using javascript

--- js/app.js
+++ js/app.js
@@ -1,22 +1,6 @@
-let btnTranslate = document.querySelector("#btn-translate");
-let txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
+console.log("This is working.");
+var userName = prompt("Enter your username: ");
 
-// console.log(txtInput);
+var welcomeMsg = "This script is working." + userName;
+alert(welcomeMsg);
 
-// btnTranslate.addEventListener("click", function clickEventHandler() {
-//     console.log("clicked!");
-//     console.log("input", txtInput.value);
-// })
-
-function clickHandler() {
-    console.log("clicked!");
-    console.log("input", txtInput.value);
-   outputDiv.innerText = "ajsjsjsjsjsjsj  " + txtInput.value;
-};
-
-
-
-btnTranslate.addEventListener("click", clickHandler);
-
-btnTranslate.addEventListener("click", clickHandler);
