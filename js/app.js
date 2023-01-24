64a8086d094fa52226f0827d2f350dde2953a555
Manas Ranjan Adha <118095682+codewithmanas@users.noreply.github.com>
2023-01-24 17:35:38 +0530

    updating main

--- js/app.js
+++ js/app.js
@@ -2,12 +2,6 @@ let btnTranslate = document.querySelector("#btn-translate");
 let txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-// console.log(txtInput);
-
-// btnTranslate.addEventListener("click", function clickEventHandler() {
-//     console.log("clicked!");
-//     console.log("input", txtInput.value);
-// })
 
 function clickHandler() {
     console.log("clicked!");
@@ -16,7 +10,5 @@ function clickHandler() {
 };
 
 
-
-btnTranslate.addEventListener("click", clickHandler);
 
 btnTranslate.addEventListener("click", clickHandler);
