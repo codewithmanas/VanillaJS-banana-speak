8a6b2fef6df8a4399d00fc8dd16a24acf1a96e4e
Manas Ranjan Adha <118095682+codewithmanas@users.noreply.github.com>
2023-01-26 15:38:27 +0530

    refactor: cleaning code in app.js

--- js/app.js
+++ js/app.js
@@ -2,9 +2,7 @@ let btnTranslate = document.querySelector("#btn-translate");
 let txtInput = document.querySelector("#txt-input");
 let outputDiv = document.querySelector("#output");
 
-// let serverURL = "https://mock-api-server.imanas96.repl.co/translate/yoda.json";
 let serverURL = "https://api.funtranslations.com/translate/minion.json";
-
 
 function getTranslationURL(text) {
     return serverURL + "?" + "text=" + text;
@@ -12,25 +10,19 @@ function getTranslationURL(text) {
 
 function errorHandler(error){
     console.log("error occured", error);
-    // alert("Server Down!");
 }
 
 function clickHandler(){
-    // outputDiv.innerText = "Bello, amee nama to? " + txtInput.value;
     let inputText = txtInput.value;
 
-    // calling server for processing
     fetch(getTranslationURL(inputText))
     .then(response => response.json())
     .then(json => {
         let translatedText = json.contents.translated;
         outputDiv.innerText = translatedText;
-        
-        console.log(json.contents.translated)})
+        })
     .catch(errorHandler)
-
 }
 
-// .addEventListener("click", callback)
 btnTranslate.addEventListener("click", clickHandler); 
 
